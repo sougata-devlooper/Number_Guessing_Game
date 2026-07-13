2a449523f74d728726435fa56cdc3835039da4ac
adarsh-ClushXD <adarshjaiswalofficial06@gmail.com>
2026-07-13 16:07:07 +0530

    Update flutter_project/lib/game_theme.dart
    Co-authored-by: sougata-devlooper <sougata.dutta2007@gmail.com>

---
+++ flutter_project/lib/game_theme.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class GameTheme {
+  static const Color lightBg = Color(0xFF4ADE80);
+  static const Color lightCard = Color(0xFFFFFFFF);
+  static const Color lightTextMain = Color(0xFF000000);
+  static const Color lightTextMuted = Color(0xFF555555);
+  static const Color lightBorder = Color(0xFF000000);
+  
+  static const Color darkBg = Color(0xFF2E0249);
+  static const Color darkCard = Color(0xFF1A1A2E);
+  static const Color darkTextMain = Color(0xFFE94560);
+  static const Color darkTextMuted = Color(0xFFA0A0A0);
+  static const Color darkBorder = Color(0xFFE94560);
+
+  static const Color accent1Light = Color(0xFFFACC15);
+  static const Color accent2Light = Color(0xFFF472B6);
+  static const Color accent3Light = Color(0xFF38BDF8);
+  static const Color accentActiveLight = Color(0xFFF97316);
+
+  static const Color accent1Dark = Color(0xFFF9A826);
+  static const Color accent2Dark = Color(0xFFFF2E63);
+  static const Color accent3Dark = Color(0xFF08D9D6);
+  static const Color accentActiveDark = Color(0xFFFF2E63);
+
+  static const Color successBgLight = Color(0xFFBBF7D0);
+  static const Color dangerBgLight = Color(0xFFFECDD3);
+  static const Color successTextLight = Color(0xFF166534);
+  static const Color dangerTextLight = Color(0xFF9F1239);
+
+  static const Color successBgDark = Color(0xFF0F341C);
+  static const Color dangerBgDark = Color(0xFF3D0A16);
+  static const Color successTextDark = Color(0xFF4ADE80);
+  static const Color dangerTextDark = Color(0xFFF87171);
+}
